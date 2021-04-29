9077d96b9b84bebbbc5ff9257eb61b41ec271f5a
nazeudon <nazeudon48112@icloud.com>
2021-04-30 00:28:04 +0900

    [WIP] add backprop

--- steps/steps01/src/main.rs
+++ steps/steps01/src/main.rs
@@ -1,9 +1,72 @@
 use ndarray::prelude::*;
 use num_traits::{Float, FromPrimitive};
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Variable<T> {
     data: T,
+    grad: Option<T>,
+}
+impl<T> Variable<T> {
+    fn new(data: T) -> Variable<T> {
+        let grad: Option<T> = None;
+        Variable { data, grad }
+    }
+}
+
+#[derive(Debug)]
+struct Square<T> {
+    input: T,
+}
+#[derive(Debug)]
+struct Exp<T> {
+    input: T,
+}
+
+trait Function<T> {
+    fn new(input: Variable<T>) -> Self;
+    fn input(&self) -> Variable<T>;
+    fn forward(&self, x: T) -> T;
+    fn backward(&self, gy: T) -> T;
+    fn call(&self) -> Variable<T> {
+        let x = self.input().data;
+        let data = self.forward(x);
+        let output = Variable::new(data);
+        output
+    }
+}
+
+impl Function<Array1<f32>> for Square<Variable<Array1<f32>>> {
+    fn new(input: Variable<Array1<f32>>) -> Square<Variable<Array1<f32>>> {
+        Square { input }
+    }
+    fn input(&self) -> Variable<Array1<f32>> {
+        self.input.clone()
+    }
+    fn forward(&self, x: Array1<f32>) -> Array1<f32> {
+        x.pow(2.0)
+    }
+    fn backward(&self, gy: Array1<f32>) -> Array1<f32> {
+        let x = self.input().data;
+        let gx = 2.0 * x * gy;
+        gx
+    }
+}
+
+impl Function<Array1<f32>> for Exp<Variable<Array1<f32>>> {
+    fn new(input: Variable<Array1<f32>>) -> Exp<Variable<Array1<f32>>> {
+        Exp { input }
+    }
+    fn input(&self) -> Variable<Array1<f32>> {
+        self.input.clone()
+    }
+    fn forward(&self, x: Array1<f32>) -> Array1<f32> {
+        x.exp()
+    }
+    fn backward(&self, gy: Array1<f32>) -> Array1<f32> {
+        let x = self.input().data;
+        let gx = x.exp() * gy;
+        gx
+    }
 }
 
 trait MathFunc<T> {
@@ -23,53 +86,35 @@ where
     }
 }
 
-trait Function<T> {
-    fn call(&self, input: Variable<T>) -> Variable<T> {
-        let x = input.data;
-        let data = self.forward(x);
-        let output = Variable { data };
-        output
-    }
-
-    fn forward(&self, x: T) -> T;
-}
-
-struct Pow;
-
-impl Function<Array1<f32>> for Pow {
-    fn forward(&self, x: Array1<f32>) -> Array1<f32> {
-        x.pow(2.0)
-    }
-}
-
-struct Exp;
-
-impl Function<Array1<f32>> for Exp {
-    fn forward(&self, x: Array1<f32>) -> Array1<f32> {
-        x.exp()
-    }
-}
-
 fn main() {
     let data = arr1(&[1.0]);
-    let x = Variable { data };
-    println!("{:?}", x);
-    println!("{:?}", x.data);
-    println!("{:?}", x.data[0]);
+    let x = Variable::new(data);
+    println!("x        : {:?}", x);
+    println!("x.data   : {:?}", x.data);
+    println!("x.data[0]: {:?}", x.data[0]);
+    println!("");
 
     let data = arr1(&[10.0]);
-    let x = Variable { data };
-    let f = Pow {};
-    let y = f.call(x);
-    println!("{:?}", y);
-    println!("{:?}", y.data);
-    println!("{:?}", y.data[0]);
+    let x = Variable::new(data);
+    let mut f = Square::new(x);
+    let y = f.call();
+    println!("y        : {:?}", y);
+    println!("y.data   : {:?}", y.data);
+    println!("y.data[0]: {:?}", y.data[0]);
+    f.input.grad = Some(f.backward(arr1(&[1.0])));
+    println!("f.input.data: {:?}", f.input.data);
+    println!("f.input.grad: {:?}", f.input.grad);
+    println!("");
 
     let data = arr1(&[10.0]);
-    let x = Variable { data };
-    let f = Exp {};
-    let y = f.call(x);
-    println!("{:?}", y);
-    println!("{:?}", y.data);
-    println!("{:?}", y.data[0]);
+    let x = Variable::new(data);
+    let mut f = Exp::new(x);
+    let y = f.call();
+    println!("y        : {:?}", y);
+    println!("y.data   : {:?}", y.data);
+    println!("y.data[0]: {:?}", y.data[0]);
+    f.input.grad = Some(f.backward(arr1(&[1.0])));
+    println!("f.input.data: {:?}", f.input.data);
+    println!("f.input.grad: {:?}", f.input.grad);
+    println!("");
 }
